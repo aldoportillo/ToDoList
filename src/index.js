aff48df0f351c6adcdaf2a9f49129ed54af5566e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-11 18:07:49 -0500

    Render sidebar

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import './style.css';
 import { createHeader } from './DOM/header';
-
-//document.body.appendChild()
-
-// import Icon from './assets/checkbox-marked-circle-plus-outline.png'
-// const header = document.createElement('header');
-// header.classList.add('header');
-// const myIcon = new Image();
-// myIcon.src = Icon;
-// const headerTitle = document.createElement('div');
-// headerTitle.textContent = "To Do List";
-
-
-// header.appendChild(myIcon);
-
-// header.appendChild(headerTitle)
-
-// document.body.appendChild(header);
+import {createSidebar } from './DOM/sidebar'
 
 createHeader();
+createSidebar();
