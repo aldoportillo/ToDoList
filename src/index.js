305c84bb0b5098e19594988aa8edeea2a83fe3fc
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-13 09:53:50 -0500

    Render a list item using my listItem class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './style.css';
 import { createHeader } from './DOM/header';
 import {createSidebar } from './DOM/sidebar'
+import {createListContainer, ListItem} from './DOM/list'
 
 createHeader();
 createSidebar();
+createListContainer();
+
+const gridContainer = document.querySelector('.grid-container')
+//gridContainer.append(createListItem());
+
+//call it
+
+let listItem1 = new ListItem(false, "Code", "8/13");
+gridContainer.append(listItem1.create());
