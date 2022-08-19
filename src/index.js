925f3367328a0553c1c7e5e17f1c0e95b231eb13
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-19 16:43:40 -0500

    Cleared index file to restart project

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import './style.css';
-import { createHeader } from './DOM/header';
-import {createSidebar } from './DOM/sidebar'
-import {createListContainer, ListItem} from './DOM/list'
-import {addFile} from './DOM/addFile'
-
-createHeader();
-createSidebar();
-createListContainer();
-
-const gridContainer = document.querySelector('.grid-container')
+import {blankProjectLoad} from './blankProjectLoad.js';
 
 
-let listItem1 = new ListItem(false, "Code", "8/13");
-gridContainer.append(listItem1.create());
-gridContainer.append(listItem1.create());
-gridContainer.append(listItem1.create());
-let listItem2 = new ListItem(true, "Sleep", "8/12")
-gridContainer.append(listItem2.create());
+import{cre}
 
-gridContainer.append(addFile());
-//I can probably add the logic for button press on this document. 
+blankProjectLoad();
 
-//Next Create a button that listens for new project and opens up modal
-//modal sends input to the class
-
-//FInd a  way to organize
