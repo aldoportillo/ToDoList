2240362aa7f78f0f440f1d1e9d06111abc1c7084
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-21 08:01:16 -0500

    Proved createToDo module is returning an object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import './style.css';
 import {blankProjectLoad} from './blankProjectLoad.js';
 import {initDomManipulation} from './initDomManipulation';
 
+import {createToDo} from './createToDO'
 
 blankProjectLoad();
 
 initDomManipulation();
+
+let toDoTable = [];
+
+const firstItem = createToDo("Homework", "To do list", "08/20/22", "low", "false");
+toDoTable.push(firstItem);
+const secondItem = createToDo("Work", "Bartend", "Today", "Urgent", "false");
+toDoTable.push(secondItem);
+const thirdItem = createToDo("Train", "MMA", "Tomorrow", "Top Priority", "false")
+toDoTable.push(thirdItem);
+console.log(firstItem)
+console.log(firstItem.Title)
+console.table(toDoTable)
