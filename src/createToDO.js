0b34a90cbbc65f42896759fe8655ae8fedc8691a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-24 09:20:45 -0500

    Make function return an array

--- src/createToDO.js
+++ src/createToDO.js
@@ -6,5 +6,5 @@ export const createToDo = (Title, Description, DueDate, Priority, CheckList) => 
     
     console.table(toDoArr);
     console.log("table rendered")
-    return {Title, Description, DueDate, Priority, CheckList} // Maybe just return object for DOM Manipulation or return the Arr
+    return toDoArr;// Maybe just return object for DOM Manipulation or return the Arr
 }
