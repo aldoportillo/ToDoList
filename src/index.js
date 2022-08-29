8abc83d59aba5b82616ffae443062c18a3794a26
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-29 17:40:23 -0500

    Create a render function to render new list directories

--- src/index.js
+++ src/index.js
@@ -6,9 +6,54 @@ const newButton = document.createElement('button')
 newButton.textContent = "New To Do"
 header.appendChild(newButton);
 document.body.appendChild(header)
+//All sidebar woek
+const sidebar = document.createElement('div');
+sidebar.classList.add('sidebar');
+const listsContainer = document.createElement('div');
+listsContainer.classList.add('lists-conatiner')
+sidebar.appendChild(listsContainer)
 
+let lists = [
+    {
+        id: 1, 
+        name: 'homework', 
+    }, {
+        id: 2,
+        name: 'job work',
+    }, {
+        id: 3,
+        name: 'tqchniques',
+    }];
+
+function render() {
+    clearElement(listsContainer)
+
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.classList.add("list-name")
+        listElement.dataset.listID = lists.id;
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement)
+    })
+}
+
+const clearElement = (element) => {
+    while(element.firstChild){
+        element.removeChild.firstChild
+    }
+}
+render()
+
+document.body.appendChild(sidebar)
+
+
+//All Card Container work
 const cardContainer = document.createElement('div');
 cardContainer.classList.add('card-container');
+
+
+
+
 
 
     class Card {
